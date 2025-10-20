6e9224d7290b1bb9a09fe00a0bdb739aa4c11a59
fahadjavedktk <124462859+fahadjavedktk@users.noreply.github.com>
2025-10-20 13:42:55 +0500

    Update student.js

--- src/config/student.js
+++ src/config/student.js
@@ -1,4 +1,4 @@
 // Edit these two fields with your details before you start.
 // They will be shown at the very top of the screen.
-export const STUDENT_NAME = "Huzaifa Farooq";
-export const REG_NO = "SP23-BSE-005";
+export const STUDENT_NAME = "Fahad Javed";
+export const REG_NO = "fa22-bse-063";
